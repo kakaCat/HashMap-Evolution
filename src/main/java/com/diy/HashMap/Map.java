4fb815f285499383b6b69a31078ac1560e32a95a
yunpeng73542@icloud.com <253834671@qq.com>
2018-03-12 18:20:19 +0800

    map

--- src/main/java/com/diy/HashMap/Map.java
+++ src/main/java/com/diy/HashMap/Map.java
@@ -3,7 +3,24 @@ package com.diy.HashMap;
 /**
  * Created by JAVA on 2018/3/11.
  */
-public interface Map {
-    jj
+public interface Map<K,V> {
+
+    /**
+     * Map双列集合 基本功能是 添加
+     */
+    V put(K k,V v);
+    /**
+     * Map双列集合 基本功能是 快速取
+     */
+    V get(K k);
+
+    /**
+     * 定义一个内部接口
+     */
+    interface Entry<K,V>{
+        K getKey();
+
+        V getValue();
+    }
 
 }
