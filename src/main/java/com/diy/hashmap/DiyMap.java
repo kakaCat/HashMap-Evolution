3f7684e232608b2c3421a4d0ae27993372e812c8
yunpeng73542@icloud.com <253834671@qq.com>
2018-03-13 08:51:56 +0800

    map

--- src/main/java/com/diy/hashmap/DiyMap.java
+++ src/main/java/com/diy/hashmap/DiyMap.java
@@ -5,12 +5,18 @@ package com.diy.hashmap;
  */
 public interface DiyMap<K,V> {
 
-    //Map双列集合 基本功能是 快速取
-    V put(K k, V v);
-    //快速取
+    /**
+     * Map双列集合 基本功能是 添加
+     */
+    V put(K k,V v);
+    /**
+     * Map双列集合 基本功能是 快速取
+     */
     V get(K k);
 
-    //定义一个内部接口
+    /**
+     * 定义一个内部接口
+     */
     interface Entry<K,V>{
         K getKey();
 
